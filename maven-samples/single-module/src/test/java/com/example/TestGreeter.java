df9af68e162299400675375657bd4ea29ccf9340
Yokesh S <145538095+Yokesh-kumaran@users.noreply.github.com>
2024-03-19 12:37:05 +0530

    Removed intention fail

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,13 +34,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-  @Test
-  public void intentionalFailing() {
-    String someone = "World";
-
-    assertTrue(true);
-  }
-
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
